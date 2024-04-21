921673d1a9dc1a5831ed42fd018e80fe2b2d9d6a
NeyOut06 <163617457+NeyOut06@users.noreply.github.com>
2024-04-21 03:08:20 -0500

    Create proyecto2.py

---
+++ proyecto2.py
@@ -0,0 +1,160 @@
+def limpiar_pantalla():
+    print("\033[H\033[J", end="")
+        
+def imprime_menu():
+    print('''                                                                                        ...........                                                                                  ''')
+    print('''                                                                                  .,:ol:;;;;;;;,;;;,,,,;;;;,,''...                                                                   ''')
+    print('''                                                                           ..;cloxOX0o:........''.',;:cclllllccc::;,;;;,,..                     *Metropolitano De Lima*              ''')
+    print('''                                                                      .':ldkKXXX00Kx;;,  ;dd;   ;oc,;clodxkkkkkkxddoooooolc:;;,'..                                                   ''')
+    print("                                                                 .,:ox0KXXK00Oxoxkl..:. .cdO0;  ;0: ...   ....',;:lodxxkkkxxxdolc;''.                                                    ")
+    print("                                                           .;cokKXXKK00Okxolccoo:. .:'  ..;:.  ;O;;xxxddoolc:;,'.........',:lO0d:,,;;''.                                                 ")
+    print("                                                       .':ldOKK00000kxdlcc:;,,,coc.   .''''',...:o,,xkO00KKK00000OOkkxool:;.  :ko,.;:. .,;.                                              ")
+    print("                                                 .':oxOKXXK0Odcllcc:;,,;;;;,'.;o;  ..,::llllc::::do,,;;::cloddkOO00KK00KK00l. ,o:.'xKd. ,c,                                              ")
+    print("                                            .':ox0KXKK00kxlcc,.,,;;;;,,,,'....;l,.;:xklcccllllooxO00kkxxdollcccc::::::clloo;  ,c;. .::. ,,;.                                             ")
+    print('''                                       .':ldO00K00Oxolc:;;,:c;',,,'....... 'c,,;'..;lc.         ....',;:clodxkkkkkkxxxdollc:,;dd,..','.,,.;'                                         ''')
+    print('''                                  .':oxOKXKOko:ccc:;;;::;,'.........       lk'...,':o;                         ....',:cclodxkO00kolooolc. ;'                                         ''')
+    print('''                             .':lxO0KK0Oxdl:c:,;:;;,,'..  ....       ... .''. ...'.;c.                          ..'.          ...,;:ccloo::'                                         ''')
+    print('''                        .':lxOKK0kdl:cc:::;,;,';;;::;;'  .;:,.   .. .',,....'.  ...;c.                        .cdol'                    .ll.                                         ''')
+    print('''                   ..;ldO0Okkxoc::c;;;,,;'...;::;;'...,. .;:,.  .'.....,....;.'c;..;c.                        ,dkxl,  ..              ...::                                          ''')
+    print('''               .;lxO00Okocc;;:;,,,,;::::;,..,..    ..;:. .,:,.  .......;....,.cXk,.;c.                        ;kOxo;. ,l.          .,::coo:..;','                                    ''')
+    print('''           'cdk00Okd:;:;,;,,;::;..,;;'.. .,,;',,,,,,,;;. .;:,.  .','...;....;.cXk,.;c.                         cOko:.  ;;        ,x00Odlddc.;;.:x'                                   ''')
+    print('''         ,lkkxo::;;;'';;'.,;;;'';;. ..',':;,:,...    .,. .;:,.  .';'...;....;.:0x,.;c.                       ',cxkdc'  ;x:.     .oNWXOxcodl'...ok'                                   ''')
+    print('''       .;::;;,,';;'',,,,;;.....':c;',,'..,,'.        .,. .,:,.  .','...;....;..'...;c.                      'd:'dOdo,. ,occ'    .dNWX0klodo'  .ok'                                   ''')
+    print('''       ,;';:,.,,,,,,....;:,'',''::.      ',,.        .,. .,:,.  .','...;....;......;c.                     'dc .oOxo;. .c''l;   .dNMN0koodd,  .lk,                                   ''')
+    print("         :..,',;,..';;''';;:.     ,:.      ';,.        .,. .;c;.  .';'...;....'.    .,c'                     'dc   lOko:. .;' .:c. .dNMN0kocdd:. .ok,                                    ")
+    print('''      ,:.''.,;,...,'    ,:.     ;:.      ';,.        .,. .;:,.  .';'...,........  .'                     'xc   .lkko:' .;, ..,l'.dNMWKOdcodc. .ok'                                   ''')
+    print('''      ,;... .;.   ,,    ,:.     ;:.      ',,'        .,. .,:,.  .',',,;:.;c;..''. .:;                   'xl    .ckOl:;. ..   ..clxNMWKOxlodl'  ,l'                                   ''')
+    print("       .;;..  .;.   ,,    ,:.    .;:.      ''.,'...'''',.  .;:,.  ,l:.,::,..,'..'.....',,,,,,,,,,,,''','.'kx,....;oOKo;;.       ..:ONWNKOxcldo,                                          ")
+    print("       .;,..  .;.   ,'    ,;. ...''''''''','..,:ccllllc::::cc:..  ,dc....,.... .,,'.''...';:clooololll:';kxcccccccclo:,;;,,''''',,.,k0kxxdoodxl.                                         ")
+    print("       .;'..  .;....''.'.''',::ccc;;:cccc:;:;:::::::::;;;;;,,'.   ,o:...';.... .''...... ..',:lllllllc';ko';ccccccccccccccccccccclc..odlodooodx,                                         ")
+    print("       .,'..',,,:cc:;;cc:;;;;;;;;:::::::::::::ccclloodddxxkkko'   .,,...';.... ... .'........;ldxxxxl.,Okcc:coollllllllllllllllloolc'.lOdoooood;                                         ")
+    print("       .,'',;;;:::ccccc:,,:clloooddxxxkkkkOOd:ckkkkxkkxxxxxdoc.   ..,...',.... .....:;,.....':dOKKKO,,O0,cx:lKXKKKKKKKKKKKKKKKKKKKKKO,.lK00000O,                                         ")
+    print("       .,';;:clooddddddc..;:lollcccccccc::::,..;;;;;;;;;;;;;,,.   .',....,.... .....,,'...  .'lxkOOl..::,dd;lOOOOOOOOOOOOOOOOOOOOOOO0o..dO0K0K0;                                         ")
+    print("       .,..',,,,,;;;;;;,..'',,,,,,,,,,,,',,,''''''''''''''''..    .',...';.... ..'..,,,'......';:c:;....,::::ccccc::cccccccccc::ccccc:...:lllod'                                         ")
+    print("       .'....'''''''',,,,,'',,,,,,,,,'''',,,,'''''''''''''''...   ..,...',.... .....,,,'..    ..............'.......''''....'''''.'...........'.                                         ")
+    print("       .' ....................................................    .',...';...  .....'.....    .'ccclllllll:............................cllllll:.                                         ")
+    print("       .'..',,coo:',,,,,,;,,,,,,,,,,,,,,,,,,,''',,,,,,;;;;;,,..;. .'....''.... .....''....    'dxlcccccccoOd;,,,,,,,,,,,,,,,'''''''''.:Oo::::cd;                                         ")
+    print("       .'.',,::;ldc::':l,;;;c;,c:,;c::loooooc:;:cc::lOXXXXKkc...  .,..''',.'lx'.....''.....   .c; .'..    ok::::;::;::;;:::::::;;::::.;kl'.   ::                                         ")
+    print("       .,.',''   ;l,'.:c.'..,..:' .'.;k0KKKKx..::';dOo;,':o00c.   ..'....,..,:......''....    .:co00d'.   lk:::::::;::;;:::::c:::cclc.,KN0:.  ::                                         ")
+    print("       .'.';;. .'';;:llooolllccc::;,,;:c:::c;..'..ox'      .dOc.  .',...';...  .....',,'....  .ckNWMK;..  lx:;:;;::;:;,,;;;:;::;::::;.,KMX:.. ::                                         ")                                 
+    print("       ....... .'.'..',;;:::::cclloddddddddoolc:,:o.     .. .lo.  .',....,.... .............  .cd0WNx'..  lk:::;;::;::;;;;;;;::;;:;;,.,Okc'.  ::                                         ")                                 
+    print("        ....'..do... ...........'',,,,;;;:::c::,.:;   .,ldo:.,l,  .''....,...  .....''.... .  .c;':c,...  lk:::;;:;;;;;;;;;;;:;;;:::;.;kl;;;;;l;                                         ")                                 
+    print("             ,ko... ......................''',..;,  .,;;;;,,':,   .,....,.... .....'....     .cc'',,,,,',kOloooodooddddddddddddddooc..;cc::::l,                                          ")                                 
+    print("              'l, ............................. .;'  ........ ,;  ..,....,.... .....,,,'....  .':ccccllcclllcccc::;,,,,'''.................. .l'              Grupo 7                   ")                                 
+    print("              .'.    ...........   ..............;'   .;cc'...,;   .,....,.... ...........      .....................  .. ............'',,',;l00c.                 Integrantes:          ")
+    print("      ..........,..         ...............  ....;.   'xKOl...;;  ..'....,...  .....''....     ................''',,;,;;::cclllooddxxxkkkkkkkkOKl                  -Kevin Coronado       ")                      
+    print("          .;'.                   ...........;.   ;O0x;...,;. .......,...  .....'..:oll:,;::llcooododdxxxxxkkkkkkkkkkkkxxxdollc::;;,,''...'dl                       -Marco                ")                      
+    print("                                             ...'.   ;OOo'.  ',. .,'.',;;...  .....'.;l'.oo...'cddxdddoollcc:::;;,,'''.................'''''',o:.                  -Anderson             ")                      
+    print("                                                      'oo,..  ,, .',,'','...,,......'.;;  ''.  ..'..................'''''',,',,;;:clodddddoolcc:;,                 -Yazid                ")                      
+    print("                                                      .,;..  ...  ..','',,,,,,'.....'.''  ..   ..',,,,,,,,,,,,,,,,'''.'cdxkkkOO00KXXNWWMWk                    Profesor: Jhonatan Sueros  \n")     
+    print('Presiona cualquier tecla para ingresar a la aplicacion...')
+
+class Estacion:
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+class Linea:
+    def __init__(self, nombre, estaciones):
+        self.nombre = nombre
+        self.estaciones = [Estacion(estacion) for estacion in estaciones]
+
+class Estaciones:
+    def __init__(self):
+        self.rutas = {
+            'A': Linea('A', ['Naranjal', 'Izaguirre', 'Pacifico', 'Independencia', 'Los Jazmines',
+                             'Tomas Valle', 'El Milagro', 'Honorio Delgado', 'UNI', 'Parque del Trabajo',
+                             'Caqueta', 'Ramon Castilla', 'Tacna', 'Jiron de la Union', 'Colmena',
+                             'Estacion Central']),
+
+            'B': Linea('B', ['Los Incas', 'Andres Belaunde', '22 de Agosto', 'Universidad', 'Naranjal', 
+                             'Izaguirre', 'Pacifico', 'Independencia', 'Los Jazmines',
+                             'Tomas Valle', 'El Milagro', 'Honorio Delgado', 'UNI', 'Parque del Trabajo',
+                             'Caqueta', 'Dos de Mayo', 'Quilca', 'Espania', 'Estacion Central', 'Estadio Nacional',
+                             'Mexico', 'Canada', 'Javier Prado', 'Canaval y Moreyra', 'Aramburu', 'Domingo Orue',
+                             'Angamos', 'Ricardo Palma', 'Benavides', '28 de Julio', 'Plaza de Flores']),
+            'C': Linea('C', ['Ramon Castilla', 'Tacna', 'Jiron de la Union', 'Colmena', 'Estacion Central',
+                             'Estadio Nacional', 'Mexico', 'Canada', 'Javier Prado', 'Canaval y Moreyra',
+                             'Aramburu', 'Domingo Orue', 'Angamos', 'Ricardo Palma', 'Benavides',
+                             '28 de Julio', 'Plaza de Flores', 'Balta', 'Bulevar', 'Estadio Union', 
+                             'Escuela Militar', 'Teran', 'Rosario de Villa', 'Matellini']),
+            'D': Linea('D', ['Naranjal', 'Izaguirre', 'Pacifico', 'Independencia', 'Los Jazmines', 'Tomas Valle',
+                             'El Milagro', 'Honorio Delgado', 'UNI', 'Parque del Trabajo', 'Caqueta', 'Dos de Mayo',
+                             'Quilca', 'Espania', 'Estacion Central'])
+        }
+
+    def MostarLinea(self, linea):
+        return self.rutas.get(linea)
+
+    def MostrarEstaciones(self, linea):
+        linea = self.MostarLinea(linea)
+        if linea:
+            print(f"Estaciones de la linea {linea.nombre}:")
+            for estacion in linea.estaciones:
+                print(estacion.nombre)
+        else:
+            print("Opcion incorrecta.")
+
+    def Ruta(self, origen, linea, destino):
+        linea = self.MostarLinea(linea)
+        if linea:
+            lineaEstaciones = linea.estaciones
+            origenEstacion = None
+            destinoEstacion = None
+            for i, estacion in enumerate(lineaEstaciones):
+                if estacion.nombre.lower() == origen.lower():
+                    origenEstacion = i
+                if estacion.nombre.lower() == destino.lower():
+                    destinoEstacion = i
+                if origenEstacion is not None and destinoEstacion is not None:
+                    break
+            if origenEstacion is None or destinoEstacion is None:
+                print("Opcion incorrecta.")
+                return
+
+            ruta = []
+            if origenEstacion < destinoEstacion:
+                ruta = lineaEstaciones[origenEstacion:destinoEstacion + 1]
+            else:
+                ruta = lineaEstaciones[destinoEstacion:origenEstacion + 1][::-1]
+
+            print("Ruta:")
+            for estacion in ruta:
+                print(estacion.nombre)
+        else:
+            print("Opcion incorrecta.")
+
+
+def main():
+    transporte = Estaciones()
+    print("================MENU=================")
+
+    while True:
+        print("\n1. Mostrar lineas")
+        print("2. Mostrar estaciones de una linea")
+        print("3. Calcular ruta")
+        print("4. Salir")
+
+        opcion = input("Ingrese una opcion: ")
+
+        if opcion == "1":
+            print("Lineas disponibles:")
+            for linea in transporte.rutas:
+                print(linea)
+        elif opcion == "2":
+            linea = input("Ingrese una linea: ")
+            transporte.MostrarEstaciones(linea.upper())
+        elif opcion == "3":
+            origen = input("Ingrese la estacion de origen: ")
+            linea = input("Ingrese la linea: ")
+            destino = input("Ingrese la estacion de destino: ")
+            transporte.Ruta(origen.lower(), linea.upper(), destino.lower())
+        elif opcion == "4":
+            print("Hasta luego")
+            break
+        else:
+            print("Opcion incorrecta.")
+
+imprime_menu()
+input()
+limpiar_pantalla()
+main()
